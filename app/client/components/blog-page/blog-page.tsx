6651f9b5395fc63965d3060a35dd5dcdaa2fccfb
aa906090 <a9066780@gmail.com>
2025-04-16 19:01:10 +0300

    added forgotten files

---
+++ app/client/components/blog-page/blog-page.tsx
@@ -0,0 +1,34 @@
+﻿import ManWithLaptop from '~/client/assets/images/man-with-laptop.png';
+
+import styles from './blog-page.module.css';
+const BlogPage = ({ redirectUrl }: { redirectUrl: string }) => {
+  const onCLick = () => {
+    // const logger = new Logger(new WinstonLogger());
+    // logger.log('hello');
+    window.location.replace(redirectUrl);
+  };
+  return (
+    <main className='max-w-xl mx-auto p-6 font-sans'>
+      <h1 className='text-3xl font-bold mb-4'>Как я выиграл 320€ за 10 минут</h1>
+      <img src={ManWithLaptop} alt='man-with-laptop' className='rounded-lg mb-4' />
+      <p className='mb-4 text-gray-700'>
+        Сейчас сложно найти способ, как можно действительно заработать в интернете. Но недавно я случайно наткнулся на
+        платформу, которая действительно выплачивает. Как раз с ее помощью мне удалось выиграть 320 евро за 10 минут.
+      </p>
+      <p className='mb-6 text-gray-700'>
+        Я зарегистрировался, и мне дали несколько бесплатных попыток. Воспользовался ними просто из любопытства и не
+        поверил своим глазам, когда увидел сумму выигрыша. После этого я продолжил, и с удивительным постоянством, доход
+        повторялся снова и снова...
+      </p>
+      <div className='bg-blue-100 p-4 rounded-lg text-center mb-4'>
+        <p className='text-sm font-medium text-gray-700'>Бонус для новых пользователей</p>
+        <p className='text-3xl font-bold my-2'>120 бесплатных вращений</p>
+        <button className='bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition' onClick={onCLick}>
+          Попробуй сейчас
+        </button>
+      </div>
+    </main>
+  );
+};
+
+export default BlogPage;
---
+++ app/client/components/main-layout/main-layout.tsx
@@ -0,0 +1,12 @@
+﻿import { PropsWithChildren } from 'react';
+
+import styles from './main-layout.module.css';
+const MainLayout = ({ children, testId = 'main-layout' }: PropsWithChildren<{ testId?: string }>) => {
+  return (
+    <div className={styles.container} data-testid={testId}>
+      {children}
+    </div>
+  );
+};
+
+export default MainLayout;
