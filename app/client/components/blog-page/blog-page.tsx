780318782c42bc77638588d30b91cb2effcf0c7c
aa906090 <a9066780@gmail.com>
2025-04-18 01:18:13 +0300

    removed redundant code

--- app/client/components/blog-page/blog-page.tsx
+++ app/client/components/blog-page/blog-page.tsx
@@ -23,24 +23,6 @@ const BlogPage = ({ redirectUrl }: { redirectUrl: string }) => {
           <TakePrize />
         </div>
       </div>
-      <h1 className='text-3xl font-bold mb-4'>Как я выиграл 320€ за 10 минут</h1>
-      <img src={ManWithLaptop} alt='man-with-laptop' className='rounded-lg mb-4' />
-      <p className='mb-4 text-gray-700'>
-        Сейчас сложно найти способ, как можно действительно заработать в интернете. Но недавно я случайно наткнулся на
-        платформу, которая действительно выплачивает. Как раз с ее помощью мне удалось выиграть 320 евро за 10 минут.
-      </p>
-      <p className='mb-6 text-gray-700'>
-        Я зарегистрировался, и мне дали несколько бесплатных попыток. Воспользовался ними просто из любопытства и не
-        поверил своим глазам, когда увидел сумму выигрыша. После этого я продолжил, и с удивительным постоянством, доход
-        повторялся снова и снова...
-      </p>
-      <div className='bg-blue-100 p-4 rounded-lg text-center mb-4'>
-        <p className='text-sm font-medium text-gray-700'>Бонус для новых пользователей</p>
-        <p className='text-3xl font-bold my-2'>120 бесплатных вращений</p>
-        <button className='bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition' onClick={onCLick}>
-          Попробуй сейчас
-        </button>
-      </div>
     </main>
   );
 };
